cdab56a7a0c8458342f08f06558ec3c08c06283a
irfanfajru <irfanfajru025@gmail.com>
2023-10-03 18:46:32 +0700

    fix bg primary

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-primary`}>{children}</body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
