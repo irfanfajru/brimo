d26367c1193234ae57598b306e893f2dc898e673
irfanfajru <irfanfajru025@gmail.com>
2023-10-03 17:46:40 +0700

    change description

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "BRImo",
-  description: "",
+  description:
+    "BRImo merupakan Aplikasi Internet dan Mobile Banking BRI Terbaru berbasis data internet yang memberikan kemudahan bagi nasabah maupun non nasabah BRI untuk dapat bertransaksi dengan User Interface dan User Experience terbaru dan fitur-fitur menarik lainnya dengan pilihan Source of Fund/ sumber dana setiap transaksi dapat menggunakan rekening Giro/ Tabungan.",
   icons: {
     icon: ["/favicon.ico?v=4"],
     apple: ["/apple-touch-icon.png?v=4"],
